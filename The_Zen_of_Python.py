1963b7f3340748eb0be0782e67071a973657af57
florije1988 <boqingfu@boqingdeMacBook-Pro.local>
2014-05-13 10:47:40 +0800

    init project

---
+++ The_Zen_of_Python.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = 'boqingfu'
+import this
+
+print this
+print this.s
+
+d = {}
+for c in (65, 97):
+    for i in range(26):
+        d[chr(i+c)] = chr((i+13) % 26 + c)
+
+print "".join([d.get(c, c) for c in this.s])
